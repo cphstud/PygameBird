f23d35107c9374eeb832575d7580615dcd0ccd56
thor <thor@cphbusiness.dk>
2022-02-14 15:25:19 +0100

    resource object

---
+++ resources/Config.py
@@ -0,0 +1,14 @@
+class Config():
+
+    def __init__(self):
+        width=600
+        height=400
+        delta=int(width/10)
+
+        xpos_bird=width-delta
+        ypos_bird=delta
+        xpos_tree=100
+        ypos_tree=100
+
+        speed_bird=1
+        framerate=120
