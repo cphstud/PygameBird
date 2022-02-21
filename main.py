ac87be86ad296be9bcbc21c49bcda3a41b5abef8
thor <thor@cphbusiness.dk>
2022-02-21 14:44:19 +0100

    rect

--- main.py
+++ main.py
@@ -24,25 +24,41 @@ clock=pygame.time.Clock()
 bg=pygame.image.load("resources/green2.jpg")
 tree=pygame.image.load("resources/tree2.png")
 bird=pygame.image.load("resources/bird.png")
+croshair=pygame.image.load("resources/crosshair.png")
+startbut=pygame.image.load("resources/start.jpeg")
+
+#  create rect
 
 #start the loop
+active = False
+counter = 0
 while True:
     # check events with for-loop
+    counter +=1
+    print(f"running {counter}")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == pygame.MOUSEMOTION:
+            croshair_rect=croshair.get_rect(center = event.pos)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                active=not active
     #put background on screen
     screen.blit(bg,(0,0))
+    if not active:
+        screen.blit(startbut,(150,100))
     # modify moving objects
-    xpos_bird=xpos_bird-speed_bird
+    else :
+        xpos_bird=xpos_bird-speed_bird
 
-    #put paint stuff on screen
-    screen.blit(tree,(100,100))
-    screen.blit(bird,(xpos_bird,ypos_bird))
+        #put paint stuff on screen
+        screen.blit(tree,(100,100))
+        screen.blit(bird,(xpos_bird,ypos_bird))
+        screen.blit(croshair,croshair_rect)
 
     #update screen
     pygame.display.update()
-    #tick the clock
-    clock.tick(120)
+#tick the clock
+clock.tick(60)
